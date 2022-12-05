17fed5cc55723c0215eb1c32cb3c309077b3503d
xiehaijie <320974775@qq.com>
2022-12-05 19:37:19 +0800

    fix: dependecy error

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['./src'],
-  format: ['esm', 'cjs', 'iife'],
+  format: ['esm', 'cjs'],
   target: 'node14',
   clean: true,
   dts: true,
