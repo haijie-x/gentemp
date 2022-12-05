959ce3bf635bcd27c0a8a15d84806f3bf9569e07
xiehaijie <320974775@qq.com>
2022-12-05 15:46:39 +0800

    refactor: ignore dist & add template

--- templates/Vanilla/tsup.config.ts
+++ templates/Vanilla/tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['./src'],
-  format: ['esm', 'cjs', 'iife'],
+  format: ['esm'],
   target: 'node14',
   clean: true,
   dts: true,
